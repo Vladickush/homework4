e78813428ff3bf02aa76c4c4a3b964fe6fbc89d5
Vladickush <u.vn@mail.ru>
2024-07-29 22:53:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+my_string = input('Введите любую строку ')
+
+# my_string = 'Привет всем участникам!'
+# print (my_string)
+print ('Количество символов в строке; ', len(my_string))
+print (my_string.upper())
+print (my_string.lower())
+print (my_string.replace(" ",""))
+print (my_string [0])
+print (my_string [-1])
